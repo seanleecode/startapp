a82a16004c7a7c329ab65f5a278b992ebf399d96
Sean Lee <skplanet@Khairuls-MacBook-Pro.local>
2016-04-05 14:00:17 +0800

    Revert "fsdfsfd"
    This reverts commit 7d5ade23f96b29ccfbf488e0e4b58a227b2134b7.

--- Gruntfile.js
+++ Gruntfile.js
@@ -76,9 +76,8 @@ module.exports = function(grunt) {
   // grunt.loadNpmTasks('grunt-contrib-qunit');
   // grunt.loadNpmTasks('grunt-contrib-jshint');
   // grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-serve');
 
   // Default task.
   // grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
-
+  grunt.registerTask('serve',[]);
 };
