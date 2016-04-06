3253e0cec4601a09ba46c690d07b5b630588fb47
Sean Lee <skplanet@Khairuls-MacBook-Pro.local>
2016-04-06 13:48:56 +0800

    updated grunt stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -71,14 +71,14 @@ module.exports = function(grunt) {
   });
 
   // These plugins provide necessary tasks.
-  // grunt.loadNpmTasks('grunt-contrib-concat');
-  // grunt.loadNpmTasks('grunt-contrib-uglify');
-  // grunt.loadNpmTasks('grunt-contrib-qunit');
-  // grunt.loadNpmTasks('grunt-contrib-jshint');
-  // grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-serve');
 
   // Default task.
   // grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
-
+  grunt.registerTask('serve', ['jshint', 'qunit', 'concat', 'uglify']);
 };
